508db91f6b4c6fb9c794c9da0f8fdac82d9d6021
l-AndyNic-l <adri.lopez@duocuc.cl>
2025-06-06 10:26:07 -0400

    Paso 1: Estructura del menú y tupla de plataformas creada

---
+++ videojuegos.py
@@ -0,0 +1,33 @@
+videojuegos = []
+
+plataformas = ( "PC", "PS5", "Xbox Series X", "Nintendo Switch" )
+
+while True:
+    print( "\n--- MENÚ DE VIDEO ---" )
+    print( "1) Registrar videojuego." )
+    print( "2) Ver videojuegos." )
+    print( "3) Modificar videjuegos." )
+    print( "4) Eliminar videojuego." )
+    print( "5) Salir." )
+
+    opcion = input( "Selcciones una opción (1-5): " )
+
+    if opcion == "1":
+        pass
+
+    elif opcion == "2":
+        pass
+
+    elif opcion == "3":
+        pass
+
+    elif opcion == "4":
+        pass
+
+    elif opcion == "5":
+        print( "Saliendo del programa." )
+        break
+
+    else:
+        print( "Opción inválida." )
+
