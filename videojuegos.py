1146c16f0cda9cb9416f3daf08b5dea5c4bed2ee
l-AndyNic-l <adri.lopez@duocuc.cl>
2025-06-06 10:55:30 -0400

    Paso 4: Modificación de videojuegos

--- videojuegos.py
+++ videojuegos.py
@@ -37,10 +37,39 @@ while True:
         print( "Videojuego registrado correctamente." )
 
     elif opcion == "2":
-        pass
+        if len( videojuegos ) == 0:
+            print( "No hay videojuegos registrados." )
+
+        else:
+            print( "\n--- LISTA DE VIDEOJUEGOS ---" )
+
+            for v in videojuegos:
+                print( f"Código: {v[ 'codigo' ]}, Nombre: {v['nombre']}, Género: {v[ 'genero' ]}, Plataforma: {v[ 'plataforma' ]}" )
 
     elif opcion == "3":
-        pass
+        codigo = int( input( "Ingrese el código del videojuego a modificar: " ) )
+        encontrado = False
+
+        for v in videojuegos:
+            if v[ "codigo" ] == codigo:
+                v[ "nombre" ] = input( "Nuevo nombre: " )
+                v[ "genero" ] = input( "Nuevo género: " )
+
+            print( "\nPlataformas disponibles: " )
+            print( "1) PC" )
+            print( "2) PS5" )
+            print( "3) Xbox Series X" )
+            print( "4) Nintendo Switch" )
+
+            plataforma_codigo = int( input( "Sleccione el número de la nueva plataforma: " ) )
+            v[ "plataforma" ] = plataformas[plataforma_codigo - 1]
+
+            print( "Videojuego modificado correctamente." )
+            encontrado = True
+            break
+
+        if not encontrado:
+            print( "vVideojuego no encontrado." )
 
     elif opcion == "4":
         pass
